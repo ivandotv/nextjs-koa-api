e8ecc877416f3a42c0621fa36487b48373732fff
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2022-05-09 12:11:38 +0200

    setup lint-staged

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-  'packages/*/src/**/*.{js,jsx,ts,tsx,json,md}': [
+  'lib/*/src/**/*.{js,jsx,ts,tsx,json,md}': [
     'eslint --fix',
     'prettier --ignore-unknown --write'
   ],
-  'packages/*/docs/**/*.{js,jsx,ts,tsx,json,md}': [
+  'lib/*/docs/**/*.{js,jsx,ts,tsx,json,md}': [
     'prettier --ignore-unknown --write'
   ],
-  'packages/*/tests/**/*.{js,jsx,ts,tsx,json}': [
+  'lib/*/tests/**/*.{js,jsx,ts,tsx,json}': [
     'eslint --fix',
     'prettier --ignore-unknown --write'
   ],
-  'demo/src/**/*.{js,jsx,ts,tsx,json,md}': [
+  'example/src/**/*.{js,jsx,ts,tsx,json,md}': [
     'eslint --fix',
     'prettier --ignore-unknown --write'
   ],
-  'demo/tests/**/*.{js,jsx,ts,tsx,json}': [
+  'example/tests/**/*.{js,jsx,ts,tsx,json}': [
     'eslint --fix',
     'prettier --ignore-unknown --write'
   ]
